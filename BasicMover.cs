441e379c89c5bc92702fadef7069be729c25502a
borrenych <85768091+borrenych@users.noreply.github.com>
2021-06-12 02:33:00 +0300

    Add files via upload

---
+++ BasicMover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doSpin)     { 
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime); 
+        }  //rotate
+
+        if (doMotion)   {
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }//move
+    }
+}
